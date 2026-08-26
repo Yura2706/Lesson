f03a7546a6405d96b9a24f6f3bd8b7cd10e67631
Yura P. <panok981@gmail.com>
2026-08-26 20:16:08 +0300

    HW

--- Lesson21/HW.js
+++ Lesson21/HW.js
@@ -21,3 +21,38 @@
 // т.е. `поле: значение` При этом: поля, которые являются 
 // функциями, нужно выводить результат работы функции 
 // (не текст функции)
+
+const product1 = {
+    name: 'notebook lenovo thinkpad',
+    price: 1283,
+    description: 'cpu intel core7, ram:16gb',
+    info() {
+        return `Prodact: ${this.name}; Price: ${this.price}; description: ${this.description}`;
+    }
+}
+
+const product2 = {
+  name: 'smartphone samsung galaxy',
+  price: 850,
+  description: 'display 6.7, 128gb, camera 50mp',
+  info() {
+    return `Prodact: ${this.name}; Price: ${this.price}; description: ${this.description}`;
+    }
+};
+
+function product(name, price, description) {
+    this.name = name;
+    this.price = price;
+    this.description = description;
+    this.info = function () {
+        return `Prodact: ${this.name}; Price: ${this.price}; description: ${this.description}`;
+    }
+}
+
+
+const products = [product,product1, product2,];
+console.log(products);
+
+
+
+
--- Lesson21/task01.js
+++ Lesson21/task01.js
@@ -1,22 +1,26 @@
-const number = [4,2,1,4,5,6,7,8,65,43,23,12,34,56,78,90,98,76,54,32,10,2,1,4];
-console.log('---sort---')
-console.log(number.sort((a,b)=>a-b));
+const number = [7,3,9,22,11,3,7,15];
+console.log('========Sort========');
+console.log(number.sort((a, b) => a - b));  
 
-number.sort((a,b)=>Math.random()-0.5);
+console.log('========Random========');
+// number.sort((a, b) =>Math.random() - 0.5);
+number.sort(() =>Math.random() - 0.5);
 console.log(number);
 
-console.log('---findIndex---')
+console.log('========MyFindIndex========');
 
 function myFindIndex(array, callback) {
     for (let i = 0; i < array.length; i++) {
         if (callback(array[i])) {
             return i;
         }
-    }
+    };
     return -1;
 }
+
 let res= myFindIndex(number, (item) => item === 22);
 console.log(res);
+
 function checkEven(item) {
     return item % 2 === 0;
 }
@@ -25,6 +29,7 @@ console.log('========CheckEven========');
 console.log(number);
 let res2 = myFindIndex(number, checkEven);
 console.log(res2);
+
 console.log('========ObjectsSort========');
 const persons = [
     { name: 'Alice', age: 30 },
@@ -32,6 +37,7 @@ const persons = [
     { name: 'Charlie', age: 35 },
     { name: 'David', age: 25 }
 ];
+
 persons.sort((a, b) => {
     if (a.age === b.age) {
         return a.name.localeCompare(b.name);
